edc6b31f89cc92cd29081c87ef5b0ec6951ce110
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2008-06-20 02:56:12 +0000

    Cleaned up the documentation and startup behavior a bit. When you start Xerblin, a StackViewer is created, then a TextViewer with the contents of the Guide word (which gets replaced by a SeqWord that just sends "show" to the Guide's TextViewer.) The same is done for the contents of the "TVGuide" word.  A TextViewer is created then replaced by a word that "show"s that TextViewer. This is the pattern I'm thinking of adopting for most of the system docs. Premade TextViewers on hardcoded string "constant" initial literals.  The user can edit them and the edits will persist, but you can't easily access the "document" objects themselves.  They appear only in the SeqWords that send them the "show" command string.

--- xerblin/startup.py
+++ xerblin/startup.py
@@ -1,15 +1,33 @@
-startup_script = ('''\
-self Guide textviewer
-"GuideViewer" setname
-self Inscribe drop
+# This is the default start up "script" for Xerblin.  The xerblin script
+# imports the startup_script string below and calls the interpret()
+# method of the new Xerblin Object with it as the argument.
+
+# If you're looking for the place where the StackViewer and the Close
+# Button are created, that's in the bin/xerblin script.
+
+startup_script = (
+    # First, create a TextViewer on the built-in word "Guide".
+    # (It's in xerblin/lib/guide.py.)
+    '''self Guide textviewer '''
+
+    # Name it "GuideViewer" and inscribe it into the Dictionary.
+    '''"GuideViewer" setname
+        self Inscribe drop '''
+    
+    # Create a TextViewer for the TextViewerGuide.
+    '''
+    self TVGuide textviewer
+    "TextViewerGuide" setname
+    self Inscribe drop
+    "hide" TextViewerGuide
+    '''
+
+    # Create the words in the built-in word "GuideWords".
+    '''self GuideWords makewords drop '''
 
 '''
+'''
 )
-##self TextViewerGuide textviewer
-##"TextViewerGuideTextViewer" setname
-##self Inscribe drop
-##
-##"hide" TextViewerGuideTextViewer
 ##
 ##self GuideWords makewords drop
 ##'''
