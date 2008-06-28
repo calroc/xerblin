4d1bdf3ff6050869da39be39fca40a879b2d5171
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2008-06-28 05:27:30 +0000

    more cleanup and docs

--- trunk/xerblin/util/stackcheckers.py
+++ trunk/xerblin/util/stackcheckers.py
@@ -1,14 +1,20 @@
-#############################################
-###    Stack Checker Mixins
-#############################################
+'''
+Stack Checker Mixins
+'''
 
 
 NUMBER_TYPES = int, float, long
 
 
+# This is a sort of abstract base class for Stack Checker Mixins.
+# It ensures that the super() calls will always succeed.
 class _urMixin(object):
     def _stackok(self, stack):
         pass
+
+
+# Helper for correct pluralization.
+_plural = lambda n: ('', 's')[n > 1]
 
 
 def StackLen(n, cache={}):
@@ -24,9 +30,9 @@ def StackLen(n, cache={}):
 
     class StackLenMixin(_urMixin):
         def _stackok(self, stack):
-            assert len(stack) >= n, \
-                   'stack too small, needs %i thing%s' % \
-                   (n, ('', 's')[n > 1])
+            assert len(stack) >= n, (
+                'stack too small, needs %i thing%s' % (n, _plural(n))
+                )
             super(StackLenMixin, self)._stackok(stack)
 
     cache[n] = StackLenMixin
@@ -36,6 +42,8 @@ def StackLen(n, cache={}):
 
 def StackType(index, type_, cache={}):
     '''
+    Return a stack item type checker Mixin that checks that the item at
+    index is of type type_.
     '''
 
     sig = (index, type_)
@@ -46,15 +54,21 @@ def StackType(index, type_, cache={}):
 
     class StackTypeMixin(_urMixin):
         def _stackok(self, stack):
-            assert isinstance(stack[index], type_), \
-                   "stack[%i] isn't %s" % sig
+            assert isinstance(stack[index], type_), (
+                "stack[%i] isn't %s" % sig
+                )
             super(StackTypeMixin, self)._stackok(stack)
 
     cache[sig] = StackTypeMixin
 
     return StackTypeMixin
 
+
 def StackHasAttr(index, attr, cache={}):
+    '''
+    Return a stack item checker Mixin that checks that the item at index
+    has an attribute named attr.
+    '''
 
     assert isinstance(attr, basestring)
 
@@ -66,13 +80,15 @@ def StackHasAttr(index, attr, cache={}):
 
     class StackHasAttrMixin(_urMixin):
         def _stackok(self, stack):
-            assert hasattr(stack[index], attr), \
-            'stack[%i] has no %s attribute' % sig
+            assert hasattr(stack[index], attr), (
+                'stack[%i] has no %s attribute' % sig
+                )
             super(StackHasAttrMixin, self)._stackok(stack)
 
     cache[sig] = StackHasAttrMixin
 
     return StackHasAttrMixin
+
 
 if __name__ == '__main__':
     class S(
