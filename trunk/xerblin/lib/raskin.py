91b0bcad842558145da85cb72929e6172b7ff9e3
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2008-07-07 02:35:30 +0000

    Making the system self documenting.

--- trunk/xerblin/lib/raskin.py
+++ trunk/xerblin/lib/raskin.py
@@ -5,6 +5,93 @@ from math import *
 class calc(StackLen(1), ExecutableWord):
     '''
     Calculate the expression on TOS.
+
+First you must put some text on the stack that describes an equation.  For example, let's say you wanted to know what three times twenty-seven was.  Select the following text, but before you let go of the mouse button press the right button once too.  (Then let go of all the buttons.)
+
+    3 * 27
+
+You should see the text of that equation on the stack.  Now Invoke calc  just right-click on the word calc right here in the text.
+
+You'll see that the text equation on the Stack has been replaced by the integer result 81.
+
+    You can also use several mathematical functions and constants, such as sin() and pi.  Try:
+
+ 2 * pi * pow(23, 2)
+
+ ...to find the area of a circle of radius 23.
+
+
+Here are the available math functions you can use in your equations:
+
+* / + -
+
+exp(x)
+    Return e**x. 
+
+log(x[, base])
+    Return the logarithm of x to the given base. If the base is not specified, return the natural logarithm of x (that is, the logarithm to base e).
+
+log10(x)
+    Return the base-10 logarithm of x. 
+
+pow(x, y)
+    Return x**y. 
+
+sqrt(x)
+    Return the square root of x. 
+
+Trigonometric functions:
+
+acos(x)
+    Return the arc cosine of x, in radians. 
+
+asin(x)
+    Return the arc sine of x, in radians. 
+
+atan(x)
+    Return the arc tangent of x, in radians. 
+
+atan2(y, x)
+    Return atan(y / x), in radians. The result is between -pi and pi. The vector in the plane from the origin to point (x, y) makes this angle with the positive X axis. The point of atan2() is that the signs of both inputs are known to it, so it can compute the correct quadrant for the angle. For example, atan(1) and atan2(1, 1) are both pi/4, but atan2(-1, -1) is -3*pi/4. 
+
+cos(x)
+    Return the cosine of x radians. 
+
+hypot(x, y)
+    Return the Euclidean norm, sqrt(x*x + y*y). This is the length of the vector from the origin to point (x, y). 
+
+sin(x)
+    Return the sine of x radians. 
+
+tan(x)
+    Return the tangent of x radians. 
+
+Angular conversion:
+
+degrees(x)
+    Converts angle x from radians to degrees. 
+
+radians(x)
+    Converts angle x from degrees to radians. 
+
+Hyperbolic functions:
+
+cosh(x)
+    Return the hyperbolic cosine of x. 
+
+sinh(x)
+    Return the hyperbolic sine of x. 
+
+tanh(x)
+    Return the hyperbolic tangent of x. 
+
+Also two mathematical constants:
+
+pi
+    The mathematical constant pi. 
+
+e
+    The mathematical constant e. 
     '''
 
     def execute(self, stack):
--- trunk/xerblin/lib/widgets/__init__.py
+++ trunk/xerblin/lib/widgets/__init__.py
@@ -44,8 +44,55 @@ class textviewer(
     StackType(1, SimpleInterpreter),
     ExecutableWord
     ):
-    '''
-    Given a ListModel on the stack open a ListViewer on it.
+    '''textviewer
+
+    Given some text and an Object on the stack open a textviewer on the text.
+
+In the Xerblin textviewer the left mouse button functions very much like most people are used to: when pressed once it sets the insertion cursor and when pressed once and dragged it traces out a selection.
+
+In addition, you can place selections onto the stack by pressing either the middle or right buttons before releasing the left one.
+
+If you click the right button the selection will be copied onto the stack, if you click the middle button the selection will be cut from the current window onto the stack.
+
+The middle button by itself pastes the current selection into the current window at the mouse position. But if you click the left button before releasing the middle button you'll paste the item on the top of the stack into the current window instead of the selection. And if you click the right button you'll paste the item on the top of the stack into the current window and also remove that item from the stack (i.e. "pop").
+
+The middle button also allows you to "grab" and scroll window contents.  If you click the middle button on a textviewer and move the mouse you'll scroll the text instead of pasting.
+
+The right button by itself will invoke the command word under the mouse.  However, if you click the left button before releasing the right button the system will instead open a viewer on the command word under the mouse. And if instead you click the middle button before releasing the right button the system will attempt to evoke the command word under the mouse, that is, it will find the word in the Dictionary and place it onto the stack. 
+
+
+Summary
+
+Right Button First = Invoke
+    or then the Left Button = Open
+    or then the Middle Button = Evoke (put it on the Stack)
+
+Left Button First = Point (set the cursor location)
+    and drag = trace out a selection (text) or copy item (list)
+        then the Right Button = Copy selected text to Stack
+        or then the Middle Button = Cut selected text to Stack
+
+Middle Button = Paste current selection to pointer.
+    or then the Left Button = Copy item on Stack to pointer.
+    or then the Right Button = "Pop" item on Stack to pointer.
+
+
+
+Summary of Summary
+(TOS means "Top Of Stack", i.e. the item on the top of the Stack.)
+
+Invoke = Right
+Open = Right, Left
+Evoke = Right, Middle
+
+Select/Point = Left
+Copy = Left, Right
+Cut = Left, Middle
+
+Paste Selection = Middle
+Paste TOS = Middle, Left
+Pop/Paste TOS = Middle, Right
+
     '''
     def execute(self, stack):
         model, interpreter = stack[:2]
