eadfe8a4d63cf481924fcbbe8f25bbee0b4ec0ba
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2009-07-17 14:55:08 +0000

    consolidating library modules.

--- trunk/xerblin/lib/false.py
+++
@@ -1,9 +0,0 @@
-from xerblin import ExecutableWord
-
-
-class false(ExecutableWord):
-    '''false
-    Push Boolean false onto the stack.
-    '''
-    def execute(self, stack):
-        stack.insert(0, False)
