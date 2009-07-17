6b27deb873133b50778b4029e5783443aba5ca99
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2009-07-17 16:13:22 +0000

    consolidating library modules.

--- trunk/xerblin/lib/programming.py
+++ trunk/xerblin/lib/programming.py
@@ -1,4 +1,5 @@
-from xerblin import ExecutableWord, Object
+from xerblin import ExecutableWord, Object, SimpleInterpreter
+from xerblin.base import BracketedExecuteWord
 from xerblin.util.stackcheckers import StackLen, StackType
 from xerblin.messaging import ListModel
 from xerblin.util.models import Variable
@@ -113,3 +114,28 @@ class get(StackLen(1), StackType(0, Variable), ExecutableWord):
     '''
     def execute(self, stack):
         stack[0] = stack[0].value
+
+
+class InvokeString(
+    StackLen(2),
+    StackType(0, basestring),
+    StackType(1, SimpleInterpreter),
+    ExecutableWord
+    ):
+    def execute(self, stack):
+        command, interp = stack[:2]
+        del stack[:2]
+        try:
+            interp.interpret(command)
+        except:
+            stack[:0] = [command, interp]
+            raise
+
+
+class InvokeWord(
+    StackLen(1),
+    StackType(0, ExecutableWord),
+    ExecutableWord
+    ):
+    def execute(self, stack):
+        BracketedExecuteWord(stack, stack.pop(0))
