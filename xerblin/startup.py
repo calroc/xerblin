74d01b57d218eda1fdf5e2670551e625efb29fe1
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2009-07-19 02:51:51 +0000

    clean up

--- xerblin/startup.py
+++ xerblin/startup.py
@@ -1,9 +1,30 @@
-# This is the default start up "script" for Xerblin.  The xerblin script
-# imports the startup_script string below and calls the interpret()
-# method of the new Xerblin Object with it as the argument.
+'''
+    Copyright (C) 2004 - 2009 Simon Forman
 
-# If you're looking for the place where the StackViewer and the Close
-# Button are created, that's in the bin/xerblin script.
+    This file is part of Xerblin.
+
+    Xerblin is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+This is the default start up "script" for Xerblin.  The xerblin script
+imports the startup_script string below and calls the interpret()
+method of the new Xerblin Object with it as the argument.
+
+If you're looking for the place where the StackViewer and the Close
+Button are created, that's in the xerblin.util.fresh module.
+'''
 from xerblin.documentation import docs
 
 
