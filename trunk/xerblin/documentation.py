5a4a82dc59832c934b43b73910ba9ed6d1556e7c
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2009-07-26 15:52:51 +0000

    slight improvement in docs

--- trunk/xerblin/documentation.py
+++ trunk/xerblin/documentation.py
@@ -17,25 +17,30 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-Helper module for startup.  The basic Documentation words are defined here.
+This module contains named documentation strings.  These strings will be made
+into TextViewers in the main interpreter.
+
+Helper module for startup.
 '''
 
-# This module contains named documentation strings.  These strings will be made
-# into TextViewers in the main interpreter.
 
 Guide = '''Welcome to Xerblin
     
-    This is the Guide.  You can close this textviewer and then open it again later by right-clicking on the word Guide in any textviewer.  You can also reopen this Guide by using the "Open Guide" button on the Xerblin main window.
+    This is the Guide.
+    
+    You can close this textviewer and then open it again later by right-clicking on the word Guide in any textviewer.  You can also reopen this Guide by using the "Open Guide" button on the Xerblin main window.
 
-    Right-clicking on a word that is also the name of a Xerblin ExecutableWord will "Invoke" that word, causing it to perform its action.  You can tell if a word is a command because it will light up orange if you try right-clicking on it.  If you click the left button before releasing the right button you will open a doc about the word.
+    Right-clicking on a word that is the name of a Xerblin ExecutableWord will "Invoke" that word, causing it to perform its action.  You can tell if a word is a command because it will light up orange if you try right-clicking on it.  
+    
+    If you click the left button before releasing the right button you will open a textviewer with information about the word.
 
-Fun words:
+    You can invoke WordList which opens a textviewer on a categorized list of some of the more useful words.  Or for a list of all the commands in the system invoke Words which opens a textviewer with a generated list of all the current words.
 
-    calc - solve equations.  Type in an equation, put it on the stack (see below) and Invoke calc
-
-    For a list of all the commands in the system Invoke Words .  Words opens a textviewer with a generated list of all the current words.  You can use open these words to read their documentation.  You can also Invoke WordList which opens a textviewer on a categorized list of some of the more useful words.
-
-    You can also put text on the stack.  First select some text, then before you let go of the left mouse button press the right button too, then release both buttons.  The text you selected will appear on the stack.  You can also use the middle button instead of the right button and the text selection will removed (cut) from the textviewer as well as put on the stack.
+    You can also put text on the stack.
+    
+    First select some text, then before you let go of the left mouse button press the right button too, then release both buttons.  The text you selected will appear on the stack.
+    
+    You can also use the middle button instead of the right button and the text selection will removed (cut) from the textviewer as well as put on the stack.
 
 Here is a summary of textviewer Mouse commands.  Open textviewer for more details about textviewers.
 
@@ -86,11 +91,13 @@ setattr setitem
 
 List Words
 ----------------------------------
+new-list
 meta unmeta
 pop push
 
 GUI Words
 ----------------------------------
+new-text
 listviewer textviewer
 
 Misc. Words
