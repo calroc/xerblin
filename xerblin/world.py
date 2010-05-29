4aeba540f21c384579eecb5a4c087e2d6d0f16a3
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2010-05-29 18:17:17 +0000

    Breaking world.py up into script and module.

--- xerblin/world.py
+++ xerblin/world.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 '''
-This script defines a proof-of-concept frame for interacting with a
-xerblin interpreter.
-
-It defines two classes: Serializer and World.
+This module defines two classes: Serializer and World.
 
 Serializer deals with saving history protocols to a pickle file and World
 deals with interacting with an interpreter in a loop.
 
 This module also defines a subclass of World called HistoryListWorld that
-tracks history in a list object.
+tracks history in a list object, and a couple of very basic views.
 
 Starting with an initial interpreter (comprised of a stack of nominal
 data and a dictionary of commands and possibly named state "variables")
@@ -270,66 +267,3 @@ class Serializer:
         '''
         self.pickler.dump(command)
         self.pickler.dump(resultant_state)
-
-
-if __name__ == "__main__":
-
-    # Create a world with the basic view function.
-    w = HistoryListWorld(view0)
-
-    # For convenience print out the commands in the dictionary at
-    # startup.
-    dictionary = w.getCurrentState()[1]
-    print ' '.join(name for name, value in items(dictionary))
-    print
-
-    # Drop into an event loop.
-    while True:
-        
-        try:
-            # Get a command and split it up.
-            command = raw_input('> ').split()
-        
-        except EOFError:
-            # User is done, quit.
-            break
-        
-        # Run the command, the World object handles all the details for
-        # us.
-        w.step(command)
-
-##
-## Example run:
-##
-## ()
-##
-## NewBranchWord NewLoopWord NewSeqWord add drop dup inscribe listwords
-## lookup mul over pick pickle rebalance sub swap tuck unpickle view
-##
-## > 23 18
-## (18, (23, ()))
-##
-## > over
-## (23, (18, (23, ())))
-##
-## > over
-## (18, (23, (18, (23, ()))))
-##
-## > swap
-## (23, (18, (18, (23, ()))))
-##
-## > sub
-## (-5, (18, (23, ())))
-##
-## > drop
-## (18, (23, ()))
-##
-## > over over sub
-## (5, (18, (23, ())))
-##
-## > drop drop drop
-## ()
-##
-## > 
-##
-##
