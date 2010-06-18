d2648fb410aeaf435b9b690aedb56f78a18b24be
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2010-06-18 05:18:59 +0000

    Added some crude functionality to the Tkinter GUI.

--- scripts/tkworld.py
+++ scripts/tkworld.py
@@ -23,31 +23,43 @@ class TkShell:
         self._updateDictionary(dictionary)
 
     def _updateStack(self, stack):
-        print 'Stack:'
-        for item in iterStack(stack):
-            print ' ', item
-        print '------\n'
+        self._update_listbox(self.stack_view, iterStack(stack))
 
     def _updateDictionary(self, dictionary):
-        print 'Dictionary:'
-        for name, value in items(dictionary):
-            print name,
-        print
+        words = (name for name, value in items(dictionary))
+        self._update_listbox(self.dictionary_view, words)
+
+    def _update_listbox(self, listbox, contents):
+        contents = list(contents)
+        listbox.delete(0, END)
+        listbox.insert(0, *contents)
+        listbox['height'] = max(10, len(contents) + 1)
 
     def _createWidgets(self, root):
-        pass
+        self.stack_view = Listbox(root)
+        self.stack_view.grid()
+        self.dictionary_view = Listbox(root)
+        self.dictionary_view.grid(row=0, column=1)
+        self.dictionary_view.bind("<Button-1>", self.command)
+
+    def command(self, event):
+        # Calculate the relative mouse coordinates as a '@x,y' string.
+        i = '@%(x)i,%(y)i' % event.__dict__
+        # Look up the word under the mouse.
+        i = self.dictionary_view.index(i)
+        word = self.dictionary_view.get(i)
+        # Execute it.
+        self.world.step([word])
 
 
 if __name__ == "__main__":
+    tk = Tk()
+    tk.title('Xerblin TkShell')
 
     w = HistoryListWorld()
+    w.step('23 18'.split())
 
-    t = TkShell(None)
+    t = TkShell(tk)
     t.setWorld(w)
 
-    while True:
-        try:
-            command = raw_input('> ').split()
-        except EOFError:
-            break
-        w.step(command)
+    tk.mainloop()
