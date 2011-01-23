5c355afaa302f25fdaa158e453b64f7cf7fb0e48
forman.simon <forman.simon@f0254e40-a41a-0410-abe3-d705822325d4>
2011-01-23 00:26:43 +0000

    More minor cleanup and renaming

--- xerblin/library.py
+++ xerblin/library.py
@@ -1,6 +1,6 @@
 # Library
 from pickle import dumps, loads
-from xerblin.base import handleSequence, handleLoop, handleBranch
+from xerblin.base import handle_sequence, handle_loop, handle_branch
 from xerblin.btree import get, insert, items, fill_tree
 from xerblin.stack import pop, push, pick_, iterStack
 
@@ -12,66 +12,58 @@ _existing.add('_existing')
 
 # Stack chatter.
 
-def dup(interpreter):
+def dup((stack, dictionary)):
     '''
     "Duplicate" the top item on the stack.
     '''
-    stack, dictionary = interpreter
     return (stack[0], stack), dictionary
 
 
-def swap(interpreter):
+def swap((stack, dictionary)):
     '''
     Reverse the order of the top two items on the stack.
     '''
-
-    stack, dictionary = interpreter
     TOS, second, stack = pop(stack, 2)
     stack = push(stack, TOS, second)
     return stack, dictionary
 
 
-def pick(interpreter):
+def pick((stack, dictionary)):
     '''
     Takes a number from the stack, counts back that many items (starting
     from zero for the top item) and puts a "duplicate" of the item found
     on the top of the stack. (So pick with 0 on the stack is thte same as
     the command "dup".)
     '''
-    stack, dictionary = interpreter
     TOS, stack = stack
     stack = pick_(stack, TOS)
     return stack, dictionary
 
 
-def tuck(interpreter):
+def tuck((stack, dictionary)):
     '''
     Put a "duplicate" of the item on the top of the stack just under the
     second item on the stack. (I.e. top, second, top.)
     '''
-    stack, dictionary = interpreter
     TOS, second, stack = pop(stack, 2)
     stack = push(stack, TOS, second, TOS)
     return stack, dictionary
 
 
-def drop(interpreter):
+def drop((stack, dictionary)):
     '''
     Remove the item on the top of the stack and discard it.
     '''
-    stack, dictionary = interpreter
     return stack[1], dictionary
 
 
-def over(interpreter):
+def over((stack, dictionary)):
     '''
     Put a "duplicate" of the second item down in the stack on the top of
     the stack. (I.e. second, top, second.)
     '''
-    stack, dictionary = interpreter
-    TOS, second, stack = pop(stack, 2)
-    stack = push(stack, second, TOS, second)
-    return stack, dictionary
+    second = stack[1][0]
+    return (second, stack), dictionary
 
 
 # Programming words.
@@ -86,7 +78,7 @@ def lookup(interpreter):
     return (word, stack), dictionary
 
 
-def inscribe(interpreter):
+def inscribe((stack, dictionary)):
     '''
     Given a name string on the top of the stack and a "combo" command
     underneath it (see NewSeqWord, NewLoopWord, and NewBranchWord for how
@@ -94,13 +86,12 @@ def inscribe(interpreter):
     dictionary under that name, replacing any previous command of that
     name.
     '''
-    stack, dictionary = interpreter
     name, word, stack = pop(stack, 2)
     dictionary = insert(dictionary, name, word)
     return stack, dictionary
 
 
-def NewSeqWord(interpreter):
+def NewSeqWord((stack, dictionary)):
     '''
     This command takes all the items on the stack and puts them into a
     tuple with the Sequence Handler function in front of them.
@@ -113,13 +104,12 @@ def NewSeqWord(interpreter):
     so on, so that the last item to run is on the top of the stack when
     you run this command.
     '''
-    stack, dictionary = interpreter
     words = tuple(reversed(list(iterStack(stack))))
-    seq = (handleSequence,) + words
+    seq = (handle_sequence,) + words
     return (seq, ()), dictionary
 
 
-def NewLoopWord(interpreter):
+def NewLoopWord((stack, dictionary)):
     '''
     This command takes all the items on the stack and puts them into a
     tuple with the Loop Handler function in front of them.
@@ -132,13 +122,12 @@ def NewLoopWord(interpreter):
     so on, so that the last item to run is on the top of the stack when
     you run this command.
     '''
-    stack, dictionary = interpreter
     words = tuple(reversed(list(iterStack(stack))))
-    loop = (handleLoop,) + words
+    loop = (handle_loop,) + words
     return (loop, ()), dictionary
 
 
-def NewBranchWord(interpreter):
+def NewBranchWord((stack, dictionary)):
     '''
     Create a new Branch command word.  A branch consumes the top item on
     the stack and does one of two things depending on its "truth" value.
@@ -147,40 +136,36 @@ def NewBranchWord(interpreter):
     the stack should be a function to use in case of "true" and the
     second should be a function to use for "false".
     '''
-    stack, dictionary = interpreter
     true, false, stack = pop(stack, 2)
-    branch = (handleBranch, true, false)
+    branch = (handle_branch, true, false)
     stack = push(stack, branch)
     return stack, dictionary
 
 
 # Math words.
 
-def add(interpreter):
+def add((stack, dictionary)):
     '''
     Add the top two items on the stack and replace them with the sum.
     '''
-    stack, dictionary = interpreter
     a, b, stack = pop(stack, 2)
     return (a + b, stack), dictionary
 
 
-def sub(interpreter):
+def sub((stack, dictionary)):
     '''
     Replace the top two items on the stack with the result of subtracting
     the top item from the second item.
     '''
-    stack, dictionary = interpreter
     a, b, stack = pop(stack, 2)
     return (b - a, stack), dictionary
 
 
-def mul(interpreter):
+def mul((stack, dictionary)):
     '''
     Replace the top two items on the stack with the result of multiplying
     them together.
     '''
-    stack, dictionary = interpreter
     a, b, stack = pop(stack, 2)
     return (a * b, stack), dictionary
 
@@ -208,7 +193,7 @@ def unpickle(interpreter):
 
 # System words
 
-def rebalance(interpreter):
+def rebalance((stack, dictionary)):
     '''
     This "rebalances" a dictionary.  It makes it more efficient to access
     commands in the dictionary if you've added a lot of new ones.
@@ -216,7 +201,6 @@ def rebalance(interpreter):
     It's a good idea to use this command before creating a pickle to
     save so that the saved pickle's dictionary is already balanced.
     '''
-    stack, dictionary = interpreter
     dictionary = fill_tree((), items(dictionary))
     return stack, dictionary
 
@@ -234,7 +218,7 @@ def listwords(interpreter):
     '''
     Print the list of words in the dictionary to stdout.
     '''
-    stack, dictionary = interpreter
+    dictionary = interpreter[1]
     for name, func in items(dictionary):
         print name
     print
